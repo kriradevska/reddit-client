5b82ca663e610ba63f85ea058b25390d83ae144b
Kristina Radevska <kriradevska@gmail.com>
2022-11-08 23:49:34 +0100

    posts and subredits components add, basic design set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import { Header } from './components/header/header';
+import { Posts } from './components/posts/posts';
+import { Subredits } from './components/subredits/subredits';
 
 function App() {
   return (
     <div className="App">
-      <Header className="header"/>
+      <Header />
+      <div className='container'>
+        <Posts />
+        <Subredits />
+      </div>
     </div>
   );
 }
---
+++ src/components/subredits/subredits.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Subredits = () => {
+
+    return(
+        <div className="subredits">
+            <h2>Subredits</h2>
+        </div>
+    )
+}
