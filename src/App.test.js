617ab9e133bd5337d49a5f2b0a198bda8495b3f3
Kristina Radevska <kriradevska@gmail.com>
2022-11-08 23:00:53 +0100

    all style move to index.css, style added to header and flexbox applied to app

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import createRoot from '@reduxjs/toolkit';
 import App from './App';
 
-import reactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  reactDOM.render(
+  ReactDOM.render(
   <Provider store={store}>
   <App />
   </Provider>, div);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './header.css';
 
 export const Header = () => {
     return (
