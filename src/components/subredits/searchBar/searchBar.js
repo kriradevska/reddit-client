65502378d007dd1b4bddb0f3b5d01290534ff2d9
Kristina Radevska <kriradevska@gmail.com>
2022-11-09 21:44:31 +0100

    add-button add

--- src/components/subredits/searchBar/searchBar.js
+++ src/components/subredits/searchBar/searchBar.js
@@ -5,8 +5,9 @@ export const SearchBar = () => {
 
     return (
         <form className="searchBar">
-            <FiSearch style={{color: '#3D3D3D', marginRight: '10px', paddingTop: '8px'}} size='25px' />
-            <input type="search" placeholder="Search Subreddits"/>
+            <FiSearch style={{color: '#3D3D3D', marginRight: 10}} size='25px' />
+            <input type="search" placeholder="Search Subreddits" style={{marginRight: 10}}/>
+            <button>Add</button>
         </form>
     )
 }
