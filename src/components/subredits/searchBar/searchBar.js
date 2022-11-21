9ab0c3004f7b23c78ac2ecadb09755b726e8556e
Kristina Radevska <kriradevska@gmail.com>
2022-11-21 17:57:14 +0100

    useEffect added

--- src/components/subredits/searchBar/searchBar.js
+++ src/components/subredits/searchBar/searchBar.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { FiSearch } from "react-icons/fi";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { addSubreddit } from "../../../app/redditSlice";
+
+import { FiSearch } from "react-icons/fi"; // for search icon
+
 
 export const SearchBar = () => {
+
+    const {subreddits} = useSelector((state) => state.subreddits)
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(addSubreddit());
+    }, [])
 
     return (
         <form className="searchBar">
             <FiSearch style={{color: '#3D3D3D', marginRight: 10}} size='25px' />
-            <input type="search" placeholder="Search Subreddits" style={{marginRight: 10}}/>
+
+            <input type="search" placeholder="Search Subreddits" style={{marginRight: 10}} />
+
             <button>Add</button>
         </form>
     )
